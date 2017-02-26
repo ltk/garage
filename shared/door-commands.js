96d894eec7b22270158dadb5c07e5d0f91b14b7a
Lawson Kurtz <lawson.kurtz@gmail.com>
2017-02-26 11:41:17 -0700

    Move more door control to API from particle

---
+++ shared/door-commands.js
@@ -0,0 +1,12 @@
+module.exports = {
+  close: {
+    location: 0,
+    complete: 'close',
+    inProgress: 'closing'
+  },
+  open: {
+    location: 100,
+    complete: 'open',
+    inProgress: 'opening'
+  }
+}
--- worker.js
+++ worker.js
@@ -2,92 +2,20 @@ require('dotenv').config()
 
 const Door = require('./api/models/door')
 const queue = require('./shared/queue')
-const Garage = require('./hardware/garage')
 
 queue.process('doorCommand', function(job, done){
   doorCommand(job.data, done)
 })
 
 // When the worker fires up, make sure door is in correct position.
-Door.fetch().then((door) => {
-  Garage.on("ready", (garage) => {
-    console.log("READY!", door)
-    if (door.status == "open") {
-      garage.commands.open(garage)
-    } else {
-      garage.commands.close(garage)
-    }
-  })
+Door.whenReady(() => {
+  Door.close()
 })
 
-
 function doorCommand(jobData, done) {
-  const operationDuration = 5.4 * 1000
+  // const operationDuration = 5.4 * 1000
   const command = jobData.command
-
-  const inflectedCommand = inflectCommand(command)
-
-  Door.fetch().then((door) => {
-    if (command == door.status) {
-      done()
-    } else {
-      const opening = command == "open"
-      const finalProgress = opening ? 0 : 1
-      const progressMultiplier = opening ? -1 : 1
-      const operationStartedAt = new Date().getTime()
-      const operationCompleteAt = operationStartedAt + operationDuration
-
-      try {
-        if (inflectedCommand.complete == 'open') {
-          Garage.commands.open(Garage)
-        } else {
-          Garage.commands.close(Garage)
-        }
-      } catch(err) {
-        console.log(err)
-      }
-
-      const progressUpdater = setInterval(() => {
-        const currentTime = new Date().getTime()
-
-        if (currentTime > operationCompleteAt) {
-          clearInterval(progressUpdater)
-
-
-          Door.set({ status: inflectedCommand.complete, progress: finalProgress }).then(() => {
-            console.log("Command Done:", inflectedCommand.complete)
-            done()
-          })
-        } else {
-          const progress = finalProgress - (((operationCompleteAt - currentTime) / operationDuration) * progressMultiplier)
-
-          Door.set({ status: inflectedCommand.inProgress, progress }).then(() => {
-            console.log("Command Progress:", inflectedCommand.inProgress, progress)
-          })
-        }
-      }, 100)
-    }
-  })
-}
-
-function inflectCommand(command) {
-  switch (command) {
-  case 'open':
-    return {
-      inProgress: 'opening',
-      complete:   'open'
-    }
-    break
-  case 'close':
-    return {
-      inProgress: 'closing',
-      complete:   'closed'
-    }
-    break
-  default:
-    return {
-      inProgress: `${command}ing`,
-      complete:   `${command}ed`
-    }
+  if (Door.isValidCommand(command)) {
+    Door[command]()
   }
 }
